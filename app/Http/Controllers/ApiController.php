23829ee39bae0b5f522e7b77bdd8f256925597c6
TheMike <themike900@pm.me>
2025-01-13 00:30:56 +0100

    Version 0.0.8.7 api updates

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -18,21 +18,29 @@ class ApiController extends Controller
         // POST-Daten in Members speichern, wenn webid noch nicht existiert
         $webid = $request->input('webid');
 
-        if (!Members::where('webid', $webid)->exists()) {
-            $member = new Members();
-            $member->webid = $request->input('webid');
-            $member->name = $request->input('name');
-            $member->firstname = $request->input('firstname');
-            $member->nickname = $request->input('nickname');
-            $member->email = $request->input('email');
-            $member->action_types = "vf,af,vt,mv,ar,abr";
-            $member->save();
+        if (DB::table('members')->where('webid', $webid)->doesntExist()) {
+
+            DB::table('members')->insert([
+                'webid' => $request->input('webid'),
+                'name' => $request->input('name'),
+                'firstname' => $request->input('firstname'),
+                'nickname' => $request->input('nickname'),
+                'email' => $request->input('email'),
+                'action_types' => "vf,af,vt,mv,ar,abr",
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+                'groups' => ''
+
+            ]);
         }
 
+        // Memberdaten aus der DB holen
+        $member = DB::table('members')->where('webid', $webid)->first();
+
         //$actions = DB::select('select * from list_actions where action_type_id in (?) order by action_date',['1, 2']);
-        $actions = DB::table('list_actions')
-            //->where('action_type_sc', 'vf')
-            //->select('action_date', 'action_type', 'crew_start_at', 'crew_end_at')
+        $actions = DB::table('actions')
+            ->where('action_type_sc', 'vf')
+            ->select('action_date', 'action_type', 'crew_start_at', 'crew_end_at')
             ->get();
 
         return response()->json($actions);
@@ -40,29 +48,16 @@ class ApiController extends Controller
 
     public function details(Request $request, int $web_id, int $action_id)
     {
-        $action = Action::find($action_id);
-        //$part =
         //$auth = $request.header('X-Auth-Token');
-        /*$action = [
-            'id' => '2',
-            'action_date' => '2025-03-20',
-            'action_type' => 'Gästefahrt',
-            'crew_start_at' => '14:00',
-            'crew_end_at' => '20:00',
-            'action_start_at' => '15:00',
-            'action_end_at' => '19:00',
-            'action_type_sc' => 'gf',
-            'action_state_sc' => 'of',
-            'reason' => '70. Geburtstag',
-            'applicant_name' => 'Max Mustermann',
-            'applicant_email' => 'max@mustermann.de',
-            'invoice_amount' => '1400',
-            'guest_count' => '20',
-            'catering_info' => 'Butter Lindner liefert',
-            'ice_info' => 'Eis vom VSaW',
-            'crew_supply' => 'Crew ist eingeladen',
-            'additional_info' => ''
-        ];*/
+
+        DB::table('action_members')->insert([
+            ['member_id' => 201,],
+            ['action_id' => 4,],
+            ['group' => 'cr',],
+            ['guests' => 0,]
+        ]);
+
+        $action = Action::find($action_id);
         $action['action_date'] = Carbon::createFromFormat('Y-m-d', $action['action_date'])->isoFormat('dddd DD.MM.');
         $action['crew_info'] = $action['crew_supply'];
         $action['service_info'] = "Catering: {$action['catering_info']},<br>Eis: {$action['ice_info']}";
@@ -81,7 +76,8 @@ class ApiController extends Controller
         $members['service'] = implode("<br>", $members['service']);
 
 
-        return response()->json(['action' => $action, "anmeldung" => $anmeldung, "members" => $members]);
+        //return response()->json(['action' => $action, "anmeldung" => $anmeldung, "members" => $members]);
+        return $request;
 
 
     }
