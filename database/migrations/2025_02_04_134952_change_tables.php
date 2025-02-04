fa49b7975ca454f46151238bcb245f20c2c5343b
TheMike <themike900@pm.me>
2025-02-04 21:20:05 +0100

    Version 0.1.19 update api

--- database/migrations/2025_02_04_134952_change_tables.php
+++ database/migrations/2025_02_04_134952_change_tables.php
@@ -13,18 +13,18 @@ return new class extends Migration
     {
 
         Schema::table('action_members', function (Blueprint $table) {
-            //$table->renameColumn('action_type', 'action_name')->change();
-            $table->dropColumn('guest');
+            $table->renameColumn('action_type', 'action_name')->change();
+            $table->dropColumn('guests');
         });
 
         Schema::table('actions', function (Blueprint $table) {
-            //$table->boolean('ac_with_wl')->default(0)->change();
+            $table->boolean('ac_with_wl')->default(0)->change();
         });
 
         Schema::table('guests', function (Blueprint $table) {
-            //$table->integer('reg_id')->nullable()->change();
-            //$table->integer('gst_action_id')->nullable()->after('reg_id')->index();
-            //$table->string('gst_state', 15)->after('gst_action_id');
+            $table->integer('reg_id')->nullable()->change();
+            $table->integer('gst_action_id')->nullable()->after('reg_id')->index();
+            $table->string('gst_state', 15)->after('gst_action_id');
         });
 
     }
