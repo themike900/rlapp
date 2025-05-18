131f61cc3153c427d33b1d37e37aa161abcc8d3c
TheMike <themike900@pm.me>
2025-05-18 20:46:29 +0200

    Version 1.8.4 update email-test-button

--- resources/views/livewire/pages/rl-members-import.blade.php
+++ resources/views/livewire/pages/rl-members-import.blade.php
@@ -5,19 +5,23 @@
             <p class="text-2xl font-bold mb-0">Mitglieder-Import von MeinVerein</p>
         </div>
 
-        <div class="flex py-4 space-x-2">
+        <div class="py-4 space-x-2">
             <main class="bg-white p-4 rounded-lg shadow-lg flex-1">
 
                 @if(session('success'))
                     <div>{{ session('success') }}</div>
-                 @endif
+                @endif
 
                 <form action="{{ route('import') }}" method="POST" enctype="multipart/form-data">
                     @csrf
                     <input id="file-upload" type="file" name="file" class="cursor-pointer space-x-2 px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700">
                     <button type="submit" class="px-4 py-2 border text-white bg-blue-600 rounded-md">Import Excel</button>
                 </form>
+                    <div class="mt-6">
+                        <button wire:click="emailTest" class="px-4 py-2 rounded-md text-white bg-gray-600">Emailtest</button>
+                    </div>
             </main>
+
         </div>
 
 
