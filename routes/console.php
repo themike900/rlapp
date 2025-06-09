b1b5f37a1e281522ca1bc9108f6b00c26d985f25
TheMike <themike900@pm.me>
2025-06-09 15:22:15 +0200

    Version 1.11.19 Update console.php service fahrtenblatt

--- routes/console.php
+++ routes/console.php
@@ -1,22 +1,18 @@
 <?php
 
 use App\Jobs\SendEmail;
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-//Artisan::command('inspire', function () {
-//    $this->comment(Inspiring::quote());
-//})->purpose('Display an inspiring quote')->hourly();
-
+// ---------------------------------------------------------------------------
 Schedule::call(function () {
     $logPath = storage_path('logs/laravel.log');
     if (file_exists($logPath)) {
         $date = now()->format('Y-m-d');
-        rename($logPath, storage_path("logs/laravel-{$date}.log"));
+        rename($logPath, storage_path("logs/laravel-$date.log"));
     }
 })->daily();
 
+// ---------------------------------------------------------------------------
 Schedule::call(function () {
     $files = glob(storage_path('logs/laravel-*.log'));
     if (count($files) > 7) { // Maximal 7 Log-Dateien behalten
@@ -24,6 +20,7 @@ Schedule::call(function () {
     }
 })->daily();
 
+// ---------------------------------------------------------------------------
 // Jeden morgen um 10:00 Fahrtenblätter für den nächsten Tag versenden
 Schedule::call(function () {
     Log::debug("--- Fahrtenblatt per Email senden now+1---");
@@ -41,9 +38,9 @@ Schedule::call(function () {
     foreach ($actions as $action) {
 
         // Nur wenn Fahrtenblatt noch nicht gesendet ist
-        if ($action->sent_fb < 2) {
+        if ($action->sent_fb < 3) {
 
-            // send Fahrtenblatt an Schiffsführer
+            // sende Fahrtenblatt an Schiffsführer
             $reg = DB::table('action_members')
                 ->where('action_id', $action->id)
                 ->where('group', 'sf')
@@ -51,10 +48,39 @@ Schedule::call(function () {
 
             if (!empty($reg)) {
                 dispatch(new SendEmail($reg->web_id, 'sf-fahrtenblatt', ['action_id' => $action->id]));
-                Log::debug("SendEmail: $reg->web_id sf-fahrtenblatt action_id={$action->id}" );
+                Log::debug("SendEmail: $reg->web_id sf-fahrtenblatt action_id=$action->id" );
             }
 
-            // send Fahrtenblatt an Service-Teilnehmer
+            // set sent_fb auf Fahrtenblatt gesendet
+            DB::table('actions')
+                ->where('id',$action->id)
+                ->update(['sent_fb' => 3]);
+
+        }
+    }
+})->dailyAt('10:00');
+
+// ---------------------------------------------------------------------------
+// Jeden morgen um 10:05 Fahrtenblätter für in 4 Tagen versenden
+Schedule::call(function () {
+    Log::debug("--- Fahrtenblatt per Email senden now+4---");
+
+    $fourDaysAhead = now()->addDays(4)->format('Y-m-d');
+    Log::debug("tomorrow: $fourDaysAhead");
+
+    $actions = DB::table('actions')
+        ->whereDate('action_date', $fourDaysAhead)
+        ->get();
+
+    Log::debug("actions: " . print_r($actions, true));
+
+    // Für alle Fahrten in 4 Tagen
+    foreach ($actions as $action) {
+
+        // Nur wenn Fahrtenblatt 4 Tge vorher noch nicht gesendet ist
+        if ($action->sent_fb < 2) {
+
+            // sende Fahrtenblatt an Service-Teilnehmer
             $sv_regs = DB::table('action_members')
                 ->where('action_id', $action->id)
                 ->where('group', 'sv')
@@ -63,7 +89,7 @@ Schedule::call(function () {
 
             foreach ($sv_regs as $sv_reg) {
                 dispatch(new SendEmail($sv_reg->web_id, 'sv-fahrtenblatt', ['action_id' => $action->id]));
-                Log::debug("SendEmail: $sv_reg->web_id sv-fahrtenblatt action_id={$action->id}" );
+                Log::debug("SendEmail: $sv_reg->web_id sv-fahrtenblatt action_id=$action->id" );
             }
 
             // set sent_fb auf Fahrtenblatt gesendet
@@ -73,12 +99,12 @@ Schedule::call(function () {
 
         }
     }
-//})->everyTwoMinutes();
-})->dailyAt('10:00');
+})->dailyAt('10:05');
 
+// ---------------------------------------------------------------------------
 // Jeden morgen um 10:10
 Schedule::call(function () {
-    Log::debug("--- Planungs-Erinnerung per Email senden now+10 ---");
+    Log::debug("--- Planungserinnerung per Email senden now+10 ---");
 
     $tenDaysAhead = now()->addDays(10)->format('Y-m-d');
     Log::debug("tenDaysAhead: $tenDaysAhead");
@@ -102,7 +128,7 @@ Schedule::call(function () {
             if (!empty($reg)) {
 
                 dispatch(new SendEmail($reg->web_id, 'sf-erinnerung', ['action_id' => $action->id]));
-                Log::debug("SendEmail: $reg->web_id sf-erinnerung action_id={$action->id}");
+                Log::debug("SendEmail: $reg->web_id sf-erinnerung action_id=$action->id");
 
                 // set sent_fb auf Erinnerung gesendet
                 DB::table('actions')
@@ -112,12 +138,10 @@ Schedule::call(function () {
             }
         }
     }
-
-//})->everyTwoMinutes();
 })->dailyAt('10:10');
 
-
-// Jeden Abend um 22:00 alle offenen und geschlossenen Aktivitäten auf durchgeführt setzen
+// ---------------------------------------------------------------------------
+// Jeden Abend um 22:00 alle offenen und geschlossenen Aktivitäten auf durchgeführt setzen und Schatzmeister benachrichtigen
 Schedule::call(function () {
     Log::debug("--- Fahrten auf durchgeführt setzen ---");
 
@@ -140,5 +164,4 @@ Schedule::call(function () {
     foreach ($actions as $action) {
         dispatch(new SendEmail($schatzmeister, 'sm-abrechnung', ['action_id' => $action->id,'preis' => $action->invoice_amount]));
     }
-
 })->dailyAt('22:00');
