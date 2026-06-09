20090c36eaa8468aee60316d62f09637b170f713
TheMike <themike900@pm.me>
2026-06-09 15:20:44 +0200

    Version 1.18.2 Update schedule 22:00

--- routes/console.php
+++ routes/console.php
@@ -181,4 +181,4 @@ Schedule::call(function () {
     foreach ($actions as $action) {
         dispatch(new SendEmail($schatzmeister, 'sm-abrechnung', ['action_id' => $action->id,'preis' => $action->invoice_amount]));
     }
-})->dailyAt('15:15');
+})->dailyAt('22:00');
